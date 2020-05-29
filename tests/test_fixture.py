2178772e610a4d827b159296f2be948fc412928e
bebleo <james.warne@outlook.com>
2020-05-29 05:37:44 -0400

    Implement AUTH LOGIN and PLAIN

--- tests/test_fixture.py
+++ tests/test_fixture.py
@@ -1,14 +1,82 @@
-import pytest
-from bebleo_smtpd_fixture.fixture import _Controller
+from email.message import EmailMessage
 from os import getenv
 
+from smtplib import SMTP
+import pytest
 
-def test_bad_certs_path(monkeypatch):
-    monkeypatch.setenv("SMTPD_USE_SSL", "True")
-    monkeypatch.setenv("SMTPD_CERTS_PATH", "")
 
-    with pytest.raises(FileNotFoundError):
-        hostname = getenv("SMTPD_HOST", "127.0.0.1")
-        port = getenv("SMPTD_PORT", "8025")
-        with _Controller(hostname, port) as controller:
-            assert controller.ssl_context is None
+@pytest.fixture
+def msg():
+    msg = EmailMessage()
+    msg["Subject"] = "Foo"
+    msg["Sender"] = "from.addr@example.org"
+    msg["To"] = "to.addr@example.org"
+    msg.set_content("foo bar")
+    return msg
+
+
+def test_init(smtpd):
+    host = getenv("SMTPD_HOST", "127.0.0.1")
+    port = int(getenv("SMTPD_PORT", "8025"))
+    assert smtpd.hostname == host
+    assert smtpd.port == port
+
+
+def test_init_ssl(mock_smtpd_use_ssl, smtpd):
+    assert bool(getenv("SMTPD_USE_SSL", "False")) is True
+    assert smtpd.ssl_context is not None
+
+
+def test_init_starttls(mock_smtpd_use_starttls, smtpd):
+    assert bool(getenv("SMTPD_USE_STARTTLS", "False")) is True
+    with SMTP(smtpd.hostname, smtpd.port) as client:
+        client.starttls()
+
+    assert smtpd.tls_context is not None
+
+
+def test_alt_port(mock_smtpd_port, smtpd):
+    assert smtpd.port == 5025
+
+
+def test_init_login(mock_smtpd_use_starttls, smtpd):
+    username = "user"
+    password = "password"
+
+    with SMTP(smtpd.hostname, smtpd.port) as client:
+        client.starttls()
+        assert client.login(username, password)
+
+
+def test_no_messages(smtpd):
+    assert len(smtpd.messages) == 0
+
+
+def test_send_message(smtpd, msg):
+    with SMTP(smtpd.hostname, smtpd.port) as client:
+        client.send_message(msg)
+
+    assert len(smtpd.messages) == 1
+
+
+def test_send_message_logged_in(mock_smtpd_use_starttls, smtpd, msg):
+    username = 'user'
+    password = 'password'
+
+    with SMTP(smtpd.hostname, smtpd.port) as client:
+        client.starttls()
+        client.login(username, password)
+        client.send_message(msg)
+
+    assert len(smtpd.messages) == 1
+
+
+def test_sendmail(smtpd):
+    from_addr = "from.addr@example.org"
+    to_addr = "to.addr@example.org"
+    msg = f"From: {from_addr}\r\nTo: {to_addr}\r\nSubject: Foo\r\n\r\nFoo bar"
+
+    with SMTP(smtpd.hostname, smtpd.port) as client:
+        client.sendmail(from_addr, to_addr, msg)
+
+    assert len(smtpd.messages) == 1
--- tests/test_smtpd.py
+++ tests/test_smtpd.py
@@ -1,56 +1,14 @@
-from email.message import EmailMessage
+import pytest
+from bebleo_smtpd_fixture.fixture import AuthController
 from os import getenv
 
-from smtplib import SMTP
 
+def test_bad_certs_path(monkeypatch):
+    monkeypatch.setenv("SMTPD_USE_SSL", "True")
+    monkeypatch.setenv("SMTPD_CERTS_PATH", "")
 
-def test_init(smtpd):
-    host = getenv("SMTPD_HOST", "127.0.0.1")
-    port = int(getenv("SMTPD_PORT", "8025"))
-    assert smtpd.hostname == host
-    assert smtpd.port == port
-
-
-def test_init_ssl(mock_smtpd_use_ssl, smtpd):
-    assert bool(getenv("SMTPD_USE_SSL", "False")) is True
-    assert smtpd.ssl_context is not None
-
-
-def test_init_starttls(mock_smtpd_use_starttls, smtpd):
-    assert bool(getenv("SMTPD_USE_STARTTLS", "False")) is True
-    with SMTP(smtpd.hostname, smtpd.port) as client:
-        client.starttls()
-
-    assert smtpd.tls_context is not None
-
-
-def test_alt_port(mock_smtpd_port, smtpd):
-    assert smtpd.port == 5025
-
-
-def test_no_messages(smtpd):
-    assert len(smtpd.messages) == 0
-
-
-def test_send_message(smtpd):
-    msg = EmailMessage()
-    msg["Subject"] = "Foo"
-    msg["Sender"] = "from.addr@example.org"
-    msg["To"] = "to.addr@example.org"
-    msg.set_content("foo bar")
-
-    with SMTP(smtpd.hostname, smtpd.port) as client:
-        client.send_message(msg)
-
-    assert len(smtpd.messages) == 1
-
-
-def test_sendmail(smtpd):
-    from_addr = "from.addr@example.org"
-    to_addr = "to.addr@example.org"
-    msg = f"From: {from_addr}\r\nTo: {to_addr}\r\nSubject: Foo\r\n\r\nFoo bar"
-
-    with SMTP(smtpd.hostname, smtpd.port) as client:
-        client.sendmail(from_addr, to_addr, msg)
-
-    assert len(smtpd.messages) == 1
+    with pytest.raises(FileNotFoundError):
+        hostname = getenv("SMTPD_HOST", "127.0.0.1")
+        port = getenv("SMPTD_PORT", "8025")
+        with AuthController(hostname, port) as controller:
+            assert controller.ssl_context is None
