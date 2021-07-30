e8dde63ac5b5a933dea5b004dfa65d2ef2c2883d
bebleo <james.warne@outlook.com>
2021-07-30 20:29:49 +0000

    Increase default ready timeout

--- smtpdfix/configuration.py
+++ smtpdfix/configuration.py
@@ -19,7 +19,7 @@ class Config():
 
         self._host = os.getenv("SMTPD_HOST")
         self._port = int(os.getenv("SMTPD_PORT", 8025))
-        self._ready_timeout = float(os.getenv("SMTPD_READY_TIMEOUT", 1.0))
+        self._ready_timeout = float(os.getenv("SMTPD_READY_TIMEOUT", 5.0))
         self._login_username = os.getenv("SMTPD_LOGIN_NAME", "user")
         self._login_password = os.getenv("SMTPD_LOGIN_PASSWORD", "password")
         self._enforce_auth = strtobool(os.getenv("SMTPD_ENFORCE_AUTH",
