49f1154e5cb043811aa7084e4c2720eb0985e7ff
James Warne <james.warne@outlook.com>
2021-11-17 21:48:02 -0500

    Modify setup
    Fix the problem with build caused by the new line in the
    description and modernize the setup.

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="smtpdfix",
-    install_requires=[
-        "aiosmtpd >= 1.3.1, <= 1.4.2",
-        "cryptography >= 3.4.4",
-        "pytest",
-        "python-dotenv",
-    ],
-    extras_require={
-        "dev": [
-            "flake8",
-            "isort",
-            "pytest-asyncio",
-            "pytest-cov",
-            "pytest-timeout",
-            "tox",
-        ],
-    },
-)
+setup()
