5dea896feb5dd7a3664f8fdda43b12b7a0261508
James Warne <15948170+bebleo@users.noreply.github.com>
2022-11-06 16:02:41 -0500

    Refactor tests to add typing

--- tests/test_fixture.py
+++ tests/test_fixture.py
@@ -1,27 +1,30 @@
 from base64 import b64encode
+from email.message import EmailMessage
 from smtplib import (SMTP, SMTP_SSL, SMTPAuthenticationError,
                      SMTPResponseException)
 from unittest import mock
 
-import pytest
+from pytest import MonkeyPatch, raises
 
 from smtpdfix import Config
+from smtpdfix.controller import AuthController
 from smtpdfix.fixture import _Authenticator
+from tests.conftest import User
 
 
-def encode(message):
+def encode(message: str) -> str:
     message_bytes = message.encode()
     base64_bytes = b64encode(message_bytes)
     base64_message = base64_bytes.decode()
     return base64_message
 
 
-def test_init(smtpd):
+def test_init(smtpd: AuthController) -> None:
     assert smtpd
     assert len(smtpd.messages) == 0
 
 
-def test_init_ssl(smtpd, msg):
+def test_init_ssl(smtpd: AuthController, msg: EmailMessage) -> None:
     smtpd.config.use_ssl = True
     with SMTP_SSL(smtpd.hostname, smtpd.port) as client:
         client.send_message(msg)
@@ -29,7 +32,7 @@ def test_init_ssl(smtpd, msg):
     assert len(smtpd.messages) == 1
 
 
-def test_AUTH_unknown_mechanism(smtpd):
+def test_AUTH_unknown_mechanism(smtpd: AuthController) -> None:
     smtpd.config.use_starttls = True
     with SMTP(smtpd.hostname, smtpd.port) as client:
         client.starttls()
@@ -38,7 +41,7 @@ def test_AUTH_unknown_mechanism(smtpd):
         assert code == 504
 
 
-def test_AUTH_LOGIN_abort(smtpd, user):
+def test_AUTH_LOGIN_abort(smtpd: AuthController, user: User) -> None:
     smtpd.config.use_starttls = True
     with SMTP(smtpd.hostname, smtpd.port) as client:
         client.starttls()
@@ -49,7 +52,7 @@ def test_AUTH_LOGIN_abort(smtpd, user):
         assert code == 501
 
 
-def test_AUTH_LOGIN_success(smtpd, user):
+def test_AUTH_LOGIN_success(smtpd: AuthController, user: User) -> None:
     smtpd.config.use_starttls = True
     username = encode(user.username)
     password = encode(user.password)
@@ -63,7 +66,7 @@ def test_AUTH_LOGIN_success(smtpd, user):
         assert code == 235
 
 
-def test_AUTH_PLAIN(smtpd, user):
+def test_AUTH_PLAIN(smtpd: AuthController, user: User) -> None:
     smtpd.config.use_starttls = True
     enc = encode(f"{user.username} {user.password}")
     cmd_text = f"PLAIN {enc}"
@@ -74,7 +77,7 @@ def test_AUTH_PLAIN(smtpd, user):
         assert code == 235
 
 
-def test_AUTH_PLAIN_no_encryption(smtpd, user):
+def test_AUTH_PLAIN_no_encryption(smtpd: AuthController, user: User) -> None:
     enc = encode(f"{user.username} {user.password}")
     cmd_text = f"PLAIN {enc}"
     with SMTP(smtpd.hostname, smtpd.port) as client:
@@ -83,7 +86,7 @@ def test_AUTH_PLAIN_no_encryption(smtpd, user):
         assert code == 538
 
 
-def test_AUTH_PLAIN_two_parts(smtpd, user):
+def test_AUTH_PLAIN_two_parts(smtpd: AuthController, user: User) -> None:
     smtpd.config.use_starttls = True
     with SMTP(smtpd.hostname, smtpd.port) as client:
         client.starttls()
@@ -95,7 +98,7 @@ def test_AUTH_PLAIN_two_parts(smtpd, user):
         assert code == 235
 
 
-def test_AUTH_PLAIN_failure(smtpd, user):
+def test_AUTH_PLAIN_failure(smtpd: AuthController, user: User) -> None:
     smtpd.config.use_starttls = True
     with SMTP(smtpd.hostname, smtpd.port) as client:
         client.starttls()
@@ -106,21 +109,21 @@ def test_AUTH_PLAIN_failure(smtpd, user):
         assert resp == b"5.7.8 Authentication credentials invalid"
 
 
-def test_alt_port(smtpd):
+def test_alt_port(smtpd: AuthController) -> None:
     smtpd.config.port = 5025
     assert smtpd.port == 5025
 
 
-def test_login(smtpd, user):
+def test_login(smtpd: AuthController, user: User) -> None:
     smtpd.config.use_starttls = True
     with SMTP(smtpd.hostname, smtpd.port) as client:
         client.starttls()
         assert client.login(user.username, user.password)
 
 
-def test_login_fail(smtpd, user):
+def test_login_fail(smtpd: AuthController, user: User) -> None:
     smtpd.config.use_starttls = True
-    with pytest.raises(SMTPAuthenticationError) as ex:
+    with raises(SMTPAuthenticationError) as ex:
         with SMTP(smtpd.hostname, smtpd.port) as client:
             client.starttls()
             client.login(user.username, user.password[:0:-1])
@@ -128,13 +131,13 @@ def test_login_fail(smtpd, user):
     assert ex.type is SMTPAuthenticationError
 
 
-def test_login_no_tls(smtpd, user):
+def test_login_no_tls(smtpd: AuthController, user: User) -> None:
     smtpd.config.auth_require_tls = False
     with SMTP(smtpd.hostname, smtpd.port) as client:
         assert client.login(user.username, user.password)
 
 
-def test_login_already_done(smtpd, user):
+def test_login_already_done(smtpd: AuthController, user: User) -> None:
     smtpd.config.enforce_auth = True
     smtpd.config.use_starttls = True
     with SMTP(smtpd.hostname, smtpd.port) as client:
@@ -147,18 +150,20 @@ def test_login_already_done(smtpd, user):
         assert resp == b"Already authenticated"
 
 
-def test_no_messages(smtpd):
+def test_no_messages(smtpd: AuthController) -> None:
     assert len(smtpd.messages) == 0
 
 
-def test_send_message(smtpd, msg):
+def test_send_message(smtpd: AuthController, msg: EmailMessage) -> None:
     with SMTP(smtpd.hostname, smtpd.port) as client:
         client.send_message(msg)
 
     assert len(smtpd.messages) == 1
 
 
-def test_send_message_logged_in(smtpd, user, msg):
+def test_send_message_logged_in(smtpd: AuthController,
+                                user: User,
+                                msg: EmailMessage) -> None:
     smtpd.config.use_starttls = True
     with SMTP(smtpd.hostname, smtpd.port) as client:
         client.starttls()
@@ -168,15 +173,16 @@ def test_send_message_logged_in(smtpd, user, msg):
     assert len(smtpd.messages) == 1
 
 
-def test_send_message_auth_not_complete(smtpd, msg):
+def test_send_message_auth_not_complete(smtpd: AuthController,
+                                        msg: EmailMessage) -> None:
     smtpd.config.enforce_auth = True
-    with pytest.raises(SMTPResponseException) as er:
+    with raises(SMTPResponseException) as er:
         with SMTP(smtpd.hostname, smtpd.port) as client:
             client.send_message(msg)
     assert er.match(r"^\(530")
 
 
-def test_sendmail(smtpd):
+def test_sendmail(smtpd: AuthController) -> None:
     from_addr = "from.addr@example.org"
     to_addr = "to.addr@example.org"
     msg = (f"From: {from_addr}\r\n"
@@ -191,7 +197,7 @@ def test_sendmail(smtpd):
 
 
 @mock.patch.object(Config, "enforce_auth", True)
-def test_mock_patch(smtpd):
+def test_mock_patch(smtpd: AuthController) -> None:
     with SMTP(smtpd.hostname, smtpd.port) as client:
         client.helo()
         code, repl = client.docmd("DATA", "")
@@ -199,7 +205,7 @@ def test_mock_patch(smtpd):
         assert repl.startswith(b"5.7.0 Authentication required")
 
 
-def test_monkeypatch(monkeypatch, smtpd):
+def test_monkeypatch(monkeypatch: MonkeyPatch, smtpd: AuthController) -> None:
     monkeypatch.setattr(smtpd.config, "enforce_auth", True)
     with SMTP(smtpd.hostname, smtpd.port) as client:
         client.helo()
@@ -212,13 +218,13 @@ class TestDefaultAuthenticator:
     _config = Config()
     _auth = _Authenticator(_config)
 
-    def test_validate(cls, user):
+    def test_validate(cls, user: User) -> None:
         assert cls._auth.validate(user.username, user.password)
 
-    def test_verify(cls, user):
-        with pytest.raises(NotImplementedError):
+    def test_verify(cls, user: User) -> None:
+        with raises(NotImplementedError):
             cls._auth.verify(user.username)
 
-    def test_get_password(cls, user):
+    def test_get_password(cls, user: User) -> None:
         password = cls._auth.get_password(user.username)
         assert password == user.password
