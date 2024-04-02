f6e1a396d034df0077c5ff96b72513132bb1e7af
bebleo <15948170+bebleo@users.noreply.github.com>
2024-04-02 11:28:39 -0400

    Use socket.gethostname not socket.getfqdn

--- smtpdfix/smtp.py
+++ smtpdfix/smtp.py
@@ -1,13 +1,63 @@
 import asyncio
 import logging
+import socket
+from ssl import SSLContext
+from typing import Any, Dict, Iterable, Optional, Union
 
-from aiosmtpd.smtp import SMTP, TLSSetupException, syntax
+from aiosmtpd.smtp import (DATA_SIZE_DEFAULT, SMTP, AuthCallbackType,
+                           AuthenticatorType, TLSSetupException, syntax)
 
 log = logging.getLogger(__name__)
 
 
 class _SMTP(SMTP):
     """Patch for the SMTP protocol from aiosmtpd."""
+    def __init__(
+            self,
+            handler: Any,
+            *args: Any,
+            data_size_limit: int = DATA_SIZE_DEFAULT,
+            enable_SMTPUTF8: bool = False,
+            decode_data: bool = False,
+            hostname: Optional[str] = None,
+            ident: Optional[str] = None,
+            tls_context: Optional[SSLContext] = None,
+            require_starttls: bool = False,
+            timeout: float = 300,
+            auth_required: bool = False,
+            auth_require_tls: bool = True,
+            auth_exclude_mechanism: Optional[Iterable[str]] = None,
+            auth_callback: Optional[AuthCallbackType] = None,
+            command_call_limit: Union[int, Dict[str, int], None] = None,
+            authenticator: Optional[AuthenticatorType] = None,
+            proxy_protocol_timeout: Optional[Union[int, float]] = None,
+            loop: Optional[asyncio.AbstractEventLoop] = None
+    ):
+        if hostname:  # pragma: no cover
+            _hostname = hostname
+        else:
+            _hostname = socket.gethostname()
+
+        super().__init__(
+            handler=handler,
+            *args,
+            data_size_limit=data_size_limit,
+            enable_SMTPUTF8=enable_SMTPUTF8,
+            decode_data=decode_data,
+            hostname=_hostname,
+            ident=ident,
+            tls_context=tls_context,
+            require_starttls=require_starttls,
+            timeout=timeout,
+            auth_required=auth_required,
+            auth_require_tls=auth_require_tls,
+            auth_exclude_mechanism=auth_exclude_mechanism,
+            auth_callback=auth_callback,
+            command_call_limit=command_call_limit,
+            authenticator=authenticator,
+            proxy_protocol_timeout=proxy_protocol_timeout,
+            loop=loop,
+        )
 
     @syntax('STARTTLS', when='tls_context')
     async def smtp_STARTTLS(self, arg: str) -> None:
